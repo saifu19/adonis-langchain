b28d3cd70c4979193fcd6a1e95a64dcd1249545a
DESKTOP-J6QB9Q1\wasam <Devpc01@connexus-technologies.com>
2024-09-23 20:32:15 +0500

    updated env files

--- app/Controllers/PagesController.ts
+++
@@ -1,7 +0,0 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
-export default class PagesController {
-  public async main({ view }: HttpContextContract) {
-    return view.render('pages/main') // Render the main page
-  }
-}
--- start/env.ts
+++ start/env.ts
@@ -15,6 +15,7 @@ export default await Env.create(new URL('../', import.meta.url), {
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   PORT: Env.schema.number(),
   APP_KEY: Env.schema.string(),
+  OPENAI_API_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.string(),
 
